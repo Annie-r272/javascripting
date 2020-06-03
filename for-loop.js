0d3487f93e970bdc84a6d78ab86a2e646fa0dacf
Annnie Rahmati <beaatris@gmail.com>
2020-06-03 17:39:53 +0000

    Initial commit with all my js work

---
+++ for-loop.js
@@ -0,0 +1,12 @@
+let total=0;
+let limit=10;
+for (let i = 0; i < limit; i++){
+  total += i ;
+};
+console.log(total);
+
+/* When this statement is used in a for loop, it can also be known as an  
+  accumulator.  Think of it like a cash register's running total while each  
+  item is scanned and added up.  For this challenge, you have 10 items and  
+  they just happen to be increasing in price by 1 each item (with the first  
+  item free!).  */
